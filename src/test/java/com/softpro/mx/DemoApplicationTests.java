345f5ace439b43d82e64cbcec7012527db7c93cc
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2023-07-19 19:14:08 -0600

    Dev

---
+++ src/test/java/com/softpro/mx/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpro.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
