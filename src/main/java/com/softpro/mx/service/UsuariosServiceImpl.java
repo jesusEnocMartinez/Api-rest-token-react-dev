3b028456a33a6b4836c776327000b77d9bc8f0a8
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2023-07-26 08:58:06 -0600

    Se agrego ruta mensualidades
    Se agrego ruta mensualidades

--- src/main/java/com/softpro/mx/service/UsuariosServiceImpl.java
+++ src/main/java/com/softpro/mx/service/UsuariosServiceImpl.java
@@ -1,11 +1,17 @@
 package com.softpro.mx.service;
 
+import com.softpro.mx.dto.MensualidadesDto;
 import com.softpro.mx.repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UsuariosServiceImpl extends UsuariosService{
+public class UsuariosServiceImpl implements UsuariosService{
     @Autowired
     private UsuariosRepository usuariosRepository;
+    @Override
+    public MensualidadesDto getMensualidadesByUsuarioId(Integer idUsuario) {
+        return usuariosRepository.MensualudadesFin(idUsuario);
+    }
+
 }
